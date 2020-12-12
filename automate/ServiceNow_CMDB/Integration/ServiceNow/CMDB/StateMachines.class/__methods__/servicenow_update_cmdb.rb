e316505ed60ddf95b17777ff62bdb0b7a0b93a11
prasadanvekar <prasad.anvekar@gmail.com>
2020-12-12 16:15:10 +0530

    Update servicenow_update_cmdb.rb

--- automate/ServiceNow_CMDB/Integration/ServiceNow/CMDB/StateMachines.class/__methods__/servicenow_update_cmdb.rb
+++ automate/ServiceNow_CMDB/Integration/ServiceNow/CMDB/StateMachines.class/__methods__/servicenow_update_cmdb.rb
@@ -111,7 +111,7 @@ end
 def get_comments
   comments = "Updated: #{Time.now.utc.strftime('%Y-%m-%d %H:%M:%S UTC')}\n"
   comments +=  "Vendor: #{@object.vendor}\n"
-  comments += "CloudForms: #{$evm.root['miq_server'].name}\n"
+  comments += "CS: #{$evm.root['miq_server'].name}\n"
   comments += "Tags: #{@object.tags.inspect}\n"
 end
 
